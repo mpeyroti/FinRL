f1eb5dcd2be78d1fe7b36257b7fce397193110fa
mpeyroti@gmail.com <mpeyroti@midinero.com.uy>
2021-08-02 23:16:20 -0300

    update data_fetch_alpaca

--- finrl/neo_finrl/alpaca/data_fetch_alpaca.py
+++ finrl/neo_finrl/alpaca/data_fetch_alpaca.py
@@ -1,10 +1,15 @@
 import alpaca_trade_api as tradeapi
 import pandas as pd
 
-def data_fetch(API_KEY, API_SECRET, APCA_API_BASE_URL,stock_list=['AAPL']
+def data_fetch(API_KEY, API_SECRET, APCA_API_BASE_URL,stock='AAPL'
                , start_date='2021-05-10',
                end_date='2021-05-10',time_interval='15Min'):
-    api = tradeapi.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, 'v2')
+    api = tradeapi.REST(
+        key_id=API_KEY, 
+        secret_key=API_SECRET, 
+        base_url=APCA_API_BASE_URL, 
+        api_version='v2'
+    )
     NY = 'America/New_York'
     start_date = pd.Timestamp(start_date, tz=NY)
     end_date = pd.Timestamp(end_date, tz=NY) + pd.Timedelta(days=1)
@@ -15,7 +20,7 @@ def data_fetch(API_KEY, API_SECRET, APCA_API_BASE_URL,stock_list=['AAPL']
         start_time=(date + pd.Timedelta('09:30:00')).isoformat()
         end_time=(date + pd.Timedelta('16:00:00')).isoformat()
         print(('Data before ') + end_time + ' is successfully fetched')
-        barset = api.get_barset(stock_list, time_interval, start=start_time,
+        barset = api._data_get_v2(APCA_API_BASE_URL, stock, time_interval, start=start_time,
                                 end=end_time, limit=500)
         if if_first_time:
             dataset = barset.df
